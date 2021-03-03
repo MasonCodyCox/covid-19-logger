6850fb0fc78ca8f00ccc7bd8b72bce8e2df7d862
MasonCodyCox <masonfraser3@googlemail.com>
2021-03-03 16:30:40 +0000

    Creates sign up page along with basic sign up form.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,6 +23,11 @@ mongo = PyMongo(app)
 def get_documents():
     documents = mongo.db.documents.find()
     return render_template("documents.html", documents=documents)
+
+
+@app.route("/sign_up", methods=["GET", "POST"])
+def sign_up():
+    return render_template("sign_up.html")
 
 
 if __name__ == "__main__":
