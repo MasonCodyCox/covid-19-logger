d4ae1e078a92d724cd339245864c1d86fe6b9788
MasonCodyCox <masonfraser3@googlemail.com>
2021-03-05 14:46:20 +0000

    Adds registration functionality. Adds styling to page titles, flash messages and navbar.

--- app.py
+++ app.py
@@ -27,6 +27,22 @@ def get_documents():
 
 @app.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already chosen. Please select something new.")
+            return redirect(url_for("sign_up"))
+
+        sign_up = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(sign_up)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Account Created!")
     return render_template("sign_up.html")
 
 
