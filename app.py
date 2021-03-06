190457901f22f6bb001f1c88f9e2059ee2e2b71d
MasonCodyCox <masonfraser3@googlemail.com>
2021-03-06 00:30:44 +0000

    Adds logout function, adds profile page and alters what pages are available to someone based on if they are logged in or not

--- app.py
+++ app.py
@@ -43,8 +43,53 @@ def sign_up():
 
         session["user"] = request.form.get("username").lower()
         flash("Account Created!")
+        return redirect(url_for("my_profile", username=session["user"]))
+
     return render_template("sign_up.html")
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Hello, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "my_profile", username=session["user"]))
+            else:
+                flash("Username and/or Password is incorrect.")
+                return redirect(url_for("login"))
+
+        else:
+            flash("Username and/or Password is incorrect..")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def my_profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("my_profile.html", username=username)
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    flash("User logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
